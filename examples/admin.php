a532f97797ee9d77874a36a4d0ef6d664a77bcf0
lajus <lajus@crans.org>
2014-01-20 23:08:13 +0100

    Some fix

--- examples/admin.php
+++ examples/admin.php
@@ -21,7 +21,7 @@ function dumpall($data, $cid) {
 	if ($tmp and $f = fopen("toshow", "w")) {
 		fwrite($f, $tmp);
 		fclose($f);
-	} else { print_r(error_get_last()); die("Writing failed"); }
+	} else { printd(error_get_last()); die("Writing failed"); }
 }
 
 if (isset($_GET["delete"]) and isset($_GET["id"]) and array_key_exists($_GET["id"], $data)) {
@@ -38,7 +38,7 @@ if (isset($_GET["type"])) {
 	$arr["min"] = $_GET["min"];
 	$arr["unit"] = $_GET["unit"];
 	$arr["voie"] = $_GET["voie"];
-	if (isset($_GET["id"]) {				// modification
+	if (isset($_GET["id"])) {				// modification
 		$arr["time"] = $data[$_GET["id"]]["time"];
 		$data[$_GET["id"]] = $arr;
 	} else {								// ajout
@@ -50,7 +50,7 @@ if (isset($_GET["type"])) {
 }
 ?>
 <html>
-<head><title>Administration du panneau d'affichage</title></head>
+<head><title>Administdation du panneau d'affichage</title></head>
 <body>
 <?php
 if (isset($_GET["modify"]) and isset($_GET["id"]) and array_key_exists($_GET["id"], $data)) {
@@ -58,68 +58,68 @@ if (isset($_GET["modify"]) and isset($_GET["id"]) and array_key_exists($_GET["id
 ?>
 <form action="admin.php" method="get">
 <table>
-<td><th>Heure</th><th>Train</th><th>num</th><th>Destination</th><th>Statut</th><th></th><th></th><th>Voie</th><th>Action</th></td>
-<td><tr>Auto</tr>
-<tr><select name="type">
+<tr><th>Heure</th><th>tdain</th><th>num</th><th>Destination</th><th>Statut</th><th></th><th></th><th>Voie</th><th>Action</th></tr>
+<tr><td>Auto</td>
+<td><select name="type">
 			<option value="THA" <?php if ($data[$id]["type"] == "TGV") echo "selected"; ?>>Thalys</option>
 			<option value="TER" <?php if ($data[$id]["type"] == "TGV") echo "selected"; ?>>TER</option>
-			<option value="TGV" <?php if ($data[$id]["type"] == "TGV") echo "selected"; ?>>TGV></option></select></tr>
-<tr><input name="num" value="<?= $data[$id]["num"] ?>" maxlength="5"/></tr>
-<tr><input name="dest" value="<?= $data[$id]["dest"] ?>" maxlength="21"></tr>
-<tr><select name="statut">
+			<option value="TGV" <?php if ($data[$id]["type"] == "TGV") echo "selected"; ?>>TGV></option></select></td>
+<td><input name="num" value="<?= $data[$id]["num"] ?>" maxlength="5"/></td>
+<td><input name="dest" value="<?= $data[$id]["dest"] ?>" maxlength="21"></td>
+<td><select name="statut">
 			<option value="A L'HEURE" <?php if ($data[$id]["statut"] == "A L'HEURE") echo "selected"; ?>>A L'HEURE</option>
 			<option value="RETARD" <?php if ($data[$id]["statut"] == "RETARD") echo "selected"; ?>>RETARD</option>
-			<option value="ANNULE" <?php if ($data[$id]["statut"] == "ANNULE") echo "selected"; ?>>ANNULE</option></select></tr>
-<tr><input name="min" value="<?= $data[$id]["min"] ?>" maxlength="2"></tr>
-<tr><select name="unit">
+			<option value="ANNULE" <?php if ($data[$id]["statut"] == "ANNULE") echo "selected"; ?>>ANNULE</option></select></td>
+<td><input name="min" value="<?= $data[$id]["min"] ?>" maxlength="2"></td>
+<td><select name="unit">
 			<option value="   " <?php if ($data[$id]["unit"] == "   ") echo "selected"; ?>></option>
 			<option value="SEC" <?php if ($data[$id]["unit"] == "SEC") echo "selected"; ?>>Secondes</option>
 			<option value="MIN" <?php if ($data[$id]["unit"] == "MIN") echo "selected"; ?>>Minutes</option>
 			<option value="HRS" <?php if ($data[$id]["unit"] == "HRS") echo "selected"; ?>>Heures</option>
 			<option value="ANS" <?php if ($data[$id]["unit"] == "ANS") echo "selected"; ?>>Ans</option>
 			<option value="SIE" <?php if ($data[$id]["unit"] == "SIE") echo "selected"; ?>>Siecles</option>
-			<option value="INF" <?php if ($data[$id]["unit"] == "INF") echo "selected"; ?>>Infini</option></select></tr>
-<tr><input name="voie" value="<?= $data[$id]["voie"] ?>" maxlength="1" type="number"></tr>
-<tr><input type="submit" value="Modifier">
-</td>
+			<option value="INF" <?php if ($data[$id]["unit"] == "INF") echo "selected"; ?>>Infini</option></select></td>
+<td><input name="voie" value="<?= $data[$id]["voie"] ?>" maxlength="1" type="number"></td>
+<td><input type="submit" value="Modifier">
+</tr>
 </table>
 <?php
 } else {
 ?>
 <form action="admin.php" method="get">
 <table>
-<td><th>Heure</th><th>Train</th><th>num</th><th>Destination</th><th>Statut</th><th></th><th></th><th>Voie</th><th>Action</th></td>
+<tr><th>Heure</th><th>tdain</th><th>num</th><th>Destination</th><th>Statut</th><th></th><th></th><th>Voie</th><th>Action</th></tr>
 <?php
 	foreach ($data as $id => $arr) {
 ?>
-<td><tr><?= $arr["time"] ?></tr><tr><?= $arr["type"] ?></tr><tr><?= $arr["num"] ?></tr><tr><?= $arr["dest"] ?></tr><tr><?= $arr["statut"] ?></tr>
-<tr><?= $arr["min"] ?></tr><tr><?= $arr["unit"] ?></tr><tr><?= $arr["voie"] ?></tr><tr><a href="?modify=t&id=<?= $id ?>">M</a><a href="?delete=t&id=<?= $id ?>">M</a></tr></td>
+<tr><td><?= $arr["time"] ?></td><td><?= $arr["type"] ?></td><td><?= $arr["num"] ?></td><td><?= $arr["dest"] ?></td><td><?= $arr["statut"] ?></td>
+<td><?= $arr["min"] ?></td><td><?= $arr["unit"] ?></td><td><?= $arr["voie"] ?></td><td><a href="?modify=t&id=<?= $id ?>">M</a><a href="?delete=t&id=<?= $id ?>">M</a></td></tr>
 <?php
 }
 ?>
-<td><tr>Auto</tr>
-<tr><select name="type">
+<tr><td>Auto</td>
+<td><select name="type">
 			<option value="THA">Thalys</option>
 			<option value="TER">TER</option>
-			<option value="TGV" selected>TGV></option></select></tr>
-<tr><input name="num" value="1" maxlength="5"/></tr>
-<tr><input name="dest" maxlength="21"></tr>
-<tr><select name="statut">
+			<option value="TGV" selected>TGV</option></select></td>
+<td><input name="num" value="1" maxlength="5"/></td>
+<td><input name="dest" maxlength="21"></td>
+<td><select name="statut">
 			<option value="A L'HEURE" selected>A L'HEURE</option>
 			<option value="RETARD">RETARD</option>
-			<option value="ANNULE">ANNULE</option></select></tr>
-<tr><input name="min" value="  " maxlength="2"></tr>
-<tr><select name="unit">
+			<option value="ANNULE">ANNULE</option></select></td>
+<td><input name="min" value="  " maxlength="2"></td>
+<td><select name="unit">
 			<option value="   " selected></option>
 			<option value="SEC">Secondes</option>
 			<option value="MIN" selected>Minutes</option>
 			<option value="HRS">Heures</option>
 			<option value="ANS">Ans</option>
 			<option value="SIE">Siecles</option>
-			<option value="INF">Infini</option></select></tr>
-<tr><input name="voie" value="1" maxlength="1" type="number"></tr>
-<tr><input type="submit" value="+">
-</td>
+			<option value="INF">Infini</option></select></td>
+<td><input name="voie" value="1" maxlength="1" type="number"></td>
+<td><input type="submit" value="+">
+</tr>
 </table>
 <?php
 }
