637f0b90654e4ae84e28c85007c6c327df923057
lajus <lajus@crans.org>
2014-01-21 01:23:12 +0100

    Revert "Test something to emulate line by line"
    This reverts commit fd4f78a0bfc2186d96bf370030e47c9e3ca3dbbd.

--- examples/ajax.js
+++ examples/ajax.js
@@ -20,26 +20,24 @@ function update() {
 			//console.debug(rep);
 			for (var i = 1; i <= 8; i++) {
 				if (typeof rep[i] === "undefined") break;
-					//console.debug('plop');
-					setTimeout( function() { 
-					var time = [rep[i].time.substr(0,2), ':',  rep[i].time.substr(3,2), ':', rep[i].time.substr(6,2)];
-					var dest = ""
-					for(var j = 0; j < rep[i].dest.length; j++) {
-						dest += (CTR.SOLARIVALUES.eletter.indexOf(rep[i].dest.charAt(j)) !== -1) ? rep[i].dest.charAt(j) : '?';
-					}
-					var min = '  ';
-					if (rep[i].min !== '  ') {
-						var tmp = Math.floor(parseInt(rep[i].min) / 5) * 5;
-						min = (tmp < 10 ? '0' : '') + tmp
-					}
-					var statut = (rep[i].statut.charAt(0) == 'A') ? ((rep[i].statut.charAt(1) == ' ') ? 'À L\'HEURE' : 'ANNULÉ   ') : 'RETARDÉ  ';
-					//console.debug([time, dest, statut, min]);
-					var num = parseInt(rep[i].num);
-					num = (( num < 10 ) ? '0000' : ( num < 100 ) ? '000' : ( num < 1000 ) ? '00' : ( num < 10000 ) ? '0' : '' ) + num;
-					display.setContent(i, [ time, rep[i].type + num, dest, statut, [min], rep[i].unit, rep[i].voie ]);
-				}, 1000*(i-1) );
+				//console.debug('plop');
+				var time = [rep[i].time.substr(0,2), ':',  rep[i].time.substr(3,2), ':', rep[i].time.substr(6,2)];
+				var dest = ""
+				for(var j = 0; j < rep[i].dest.length; j++) {
+					dest += (CTR.SOLARIVALUES.eletter.indexOf(rep[i].dest.charAt(j)) !== -1) ? rep[i].dest.charAt(j) : '?';
+				}
+				var min = '  ';
+				if (rep[i].min !== '  ') {
+					var tmp = Math.floor(parseInt(rep[i].min) / 5) * 5;
+					min = (tmp < 10 ? '0' : '') + tmp
+				}
+				var statut = (rep[i].statut.charAt(0) == 'A') ? ((rep[i].statut.charAt(1) == ' ') ? 'À L\'HEURE' : 'ANNULÉ   ') : 'RETARDÉ  ';
+				//console.debug([time, dest, statut, min]);
+				var num = parseInt(rep[i].num);
+				num = (( num < 10 ) ? '0000' : ( num < 100 ) ? '000' : ( num < 1000 ) ? '00' : ( num < 10000 ) ? '0' : '' ) + num;
+				display.setContent(i, [ time, rep[i].type + num, dest, statut, [min], rep[i].unit, rep[i].voie ]);
 			}
-			setTimeout(update, 10000);
+			setTimeout(update, 2000);
         } else if (xhr.readyState == 4) {
 			setTimeout(update, 500);
 		}
